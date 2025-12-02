e34f99470fe3e68cdafed9273b38a82145315bdf
Gosho-oss <georgi@wallscapebg.com>
2025-12-02 22:18:46 +0200

    Clean up unnecessary files

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// Vercel serverless function wrapper
-const path = require('path');
-
-// Set the correct path for production build
-process.chdir(path.join(__dirname, '..'));
-
-// Import and export the Express app
-module.exports = require('../dist/index.cjs');
